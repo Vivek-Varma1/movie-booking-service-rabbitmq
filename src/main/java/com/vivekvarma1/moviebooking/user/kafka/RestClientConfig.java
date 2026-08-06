3c58526013abeaf345a6b75fac3446682c692039
vivek <vivekvarmanizampanam@gmail.com>
2026-08-06 12:43:37 +0530

    bug fixes url

--- src/main/java/com/vivekvarma1/moviebooking/user/kafka/RestClientConfig.java
+++ src/main/java/com/vivekvarma1/moviebooking/user/kafka/RestClientConfig.java
@@ -1,16 +1,19 @@
 package com.vivekvarma1.moviebooking.user.kafka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-
 @Configuration
 public class RestClientConfig {
+
+    @Value("${email.service.url}")
+    private String emailServiceUrl;
 
     @Bean
     public RestClient emailServiceRestClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(emailServiceUrl)
                 .build();
     }
 }
