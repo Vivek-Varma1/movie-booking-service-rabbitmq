3deae57db3f08430dc113b5972f8f326b9dfb3af
vivek <vivekvarmanizampanam@gmail.com>
2026-08-07 22:54:25 +0530

    fixed cold start

--- src/main/java/com/vivekvarma1/moviebooking/security/SecurityConfig.java
+++ src/main/java/com/vivekvarma1/moviebooking/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
-
+                        .requestMatchers("/ping").permitAll()
                         // Authentication APIs
                         .requestMatchers("/api/auth/**","/api/tickets/**").permitAll()
 
