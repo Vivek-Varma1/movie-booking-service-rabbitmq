e834f94c8b7b4ec15d9c6c880d01c4513375199a
vivek <vivekvarmanizampanam@gmail.com>
2026-08-08 23:49:36 +0530

    added Cloudinary Uploads for movie

--- src/main/java/com/vivekvarma1/moviebooking/event/controller/MovieController.java
+++ src/main/java/com/vivekvarma1/moviebooking/event/controller/MovieController.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/movies")
 @RequiredArgsConstructor
