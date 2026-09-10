e96cc6dbc208c035f333c598380d7e7c3f881eed
vivek <vivekvarmanizampanam@gmail.com>
2026-09-10 22:03:16 +0530

    Modified the RabbitMqConfig

--- src/main/java/com/vivekvarma1/moviebooking/config/RabbitMQConfig.java
+++ src/main/java/com/vivekvarma1/moviebooking/config/RabbitMQConfig.java
@@ -1,8 +1,8 @@
 package com.vivekvarma1.moviebooking.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-//import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +20,7 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    MessageConverter jsonConverter() {
-        return new Jackson2JsonMessageConverter();
+    MessageConverter jsonConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
